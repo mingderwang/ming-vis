5785e9800977dc05da11784071ddaebe8c66b6df
Ming-der Wang <ming@log4analytics.com>
2017-03-21 18:55:11 +0800

    no error, no display, with Data/Options menu in ming-vis

---
+++ public/ming_controller.js
@@ -0,0 +1,10 @@
+import _ from 'lodash';
+import uiModules from 'ui/modules';
+import MingCloud from 'plugins/ming/ming';
+import AggConfigResult from 'ui/vis/agg_config_result';
+import FilterBarFilterBarClickHandlerProvider from 'ui/filter_bar/filter_bar_click_handler';
+
+const module = uiModules.get('kibana/ming', ['kibana']);
+module.controller('KbnTagCloudController', function ($scope, $element, Private, getAppState) {
+  
+});
--- public/ming_vis.js
+++ public/ming_vis.js
@@ -6,9 +6,11 @@ import VisSchemasProvider from 'ui/vis/schemas';
 import mingCloudTemplate from 'plugins/ming/ming_controller.html';
 import visTypes from 'ui/registry/vis_types';
 
+
 visTypes.register(
- function TagCloudProvider(Private) {
+ function MingCloudProvider(Private) {
    const TemplateVisType = Private(TemplateVisTypeTemplateVisTypeProvider);
+   const Schemas = Private(VisSchemasProvider);
    return new TemplateVisType({
     name: 'ming',
     title: 'ming cloud',
@@ -16,7 +18,37 @@ visTypes.register(
     description: 'A d3.js representation of iPOC data metrix.',
     icon: 'fa-rocket',
     template: mingCloudTemplate,
-    
+    params: {
+      defaults: {
+        scale: 'linear'
+      },
+      scales: ['linear', 'log', 'square root']
+    },
+    schemas: new Schemas([
+      {
+        group: 'metrics',
+        name: 'metric',
+        title: 'Size',
+        min: 1,
+        max: 1,
+        aggFilter: ['!std_dev', '!percentiles', '!percentile_ranks', '!derivative'],
+        defaults: [
+          { schema: 'metric', type: 'count' }
+        ]
+      },
+      {
+        group: 'buckets',
+        name: 'segment',
+        icon: 'fa fa-rocket',
+        title: 'Ming',
+        min: 1,
+        max: 1,
+        aggFilter: ['terms']
+      }
+    ])
    });
  }
 );
+
+// export the provider so that the visType can be required with Private()
+//export default MingCloudProvider;
